686dd2c149d88637f4de1eec057c4bd16fc8f937
Krunal01 <parmarkrunal09101@gmail.com>
2022-12-09 23:03:44 +0530

    simple useState hook toggle

--- src/UseState.js
+++ src/UseState.js
@@ -1,18 +1,45 @@
 import react, { useState } from "react";
-function UseState() {
-  let [name, setName] = useState("useState hook");
-  const onClickHandler = () => {
-    let val = name;
-    (val == "useState hook") ?
-      setName("useState Hook in React"):
-      setName("useState hook");
-    
+export default function UseState() {
+  const info = [
+    {
+      id: 1,
+      name: "kumar",
+      age: 31,
+    },
+    {
+      id: 2,
+      name: "tech",
+      age: 26,
+    },
+  ];
+  const [array, setArray] = useState(info);
+  const clearArray = () => {
+    setArray([]);
   };
   return (
     <>
-      <h1>{name}</h1>
-      <button onClick={onClickHandler}>Click Me</button>
+      <div>
+        {array.map((e) => {
+          return `Name is ${e.name} and Age is ${e.age} with Id ${e.id} `;
+        })}
+      </div>
+      <button onClick={clearArray}>Clear</button>
     </>
   );
 }
-export default UseState;
+//   <h1>useState with Array</h1>
+
+//  UseState;
+
+// console.log(e.name, e.age, e.id);
+
+// let [name, setName] = useState("useState hook");
+// const onClickHandler = () => {
+//   let val = name;
+//   (val == "useState hook") ?
+//     setName("useState Hook in React"):
+//     setName("useState hook");
+
+// };
+// <h1>{name}</h1>
+//       <button onClick={onClickHandler}>Click Me</button>
